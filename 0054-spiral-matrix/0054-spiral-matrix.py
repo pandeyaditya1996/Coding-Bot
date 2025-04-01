2b4448f928448b7d085a7fc9582d0f82d62bf216
pandeyaditya1996 <144877206+pandeyaditya1996@users.noreply.github.com>
2025-04-01 00:48:43 -0700

    0054-SPIRAL-MATRIX Stats: Time: 0 ms (100%), Space: 18 MB (18.84%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,8 +4,8 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
 
+        ans = []
         count = 0
-        total = rows*cols
 
         top = 0
         bottom = rows - 1
@@ -13,14 +13,10 @@ class Solution:
         left = 0
         right = cols - 1
 
-        ans = []
-
         while(count<rows*cols):
 
-            for i in range(left,right+1):
-                ans.append(matrix[top][i])
-                count+=1
-
+            for j in range(left,right+1):
+                ans.append(matrix[top][j])
             top+=1
 
             if top>bottom:
@@ -28,26 +24,20 @@ class Solution:
 
             for i in range(top,bottom+1):
                 ans.append(matrix[i][right])
-                count+=1
-
             right-=1
 
             if left>right:
-                break            
+                break
 
-            for i in range(right,left - 1, -1):
-                ans.append(matrix[bottom][i])
-                count+=1
-
+            for j in range(right, left - 1, -1):
+                ans.append(matrix[bottom][j])
             bottom-=1
 
             if top>bottom:
                 break
 
-            for i in range(bottom,top - 1, -1):
+            for i in range(bottom, top - 1, -1):
                 ans.append(matrix[i][left])
-                count+=1
-
             left+=1
 
             if left>right:
