b7539a58c3f47325ae15bd77665d24dd6a9888dc
pandeyaditya1996 <144877206+pandeyaditya1996@users.noreply.github.com>
2025-04-22 20:19:17 -0700

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 131 ms (26.74%), Space: 26.8 MB (78.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        ans = float('-inf')
+
         high = float('-inf')
+        ans = float('-inf')
+
         for price in prices[::-1]:
             high = max(high, price)
-            ans = max(ans, high - price)
+            ans = max(ans, high - price)        
 
         return ans
